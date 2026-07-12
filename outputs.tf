7e6a245ff07d7be7b3dba6119e7b630e88c178b9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "container_registry_webhooks" {
-  description = "All container_registry_webhook resources"
-  value       = azurerm_container_registry_webhook.container_registry_webhooks
-}
 output "container_registry_webhooks_actions" {
-  description = "List of actions values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.actions]
+  description = "Map of actions values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.actions }
 }
 output "container_registry_webhooks_custom_headers" {
-  description = "List of custom_headers values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.custom_headers]
+  description = "Map of custom_headers values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.custom_headers }
 }
 output "container_registry_webhooks_location" {
-  description = "List of location values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.location]
+  description = "Map of location values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.location }
 }
 output "container_registry_webhooks_name" {
-  description = "List of name values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.name]
+  description = "Map of name values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.name }
 }
 output "container_registry_webhooks_registry_name" {
-  description = "List of registry_name values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.registry_name]
+  description = "Map of registry_name values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.registry_name }
 }
 output "container_registry_webhooks_resource_group_name" {
-  description = "List of resource_group_name values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.resource_group_name]
+  description = "Map of resource_group_name values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.resource_group_name }
 }
 output "container_registry_webhooks_scope" {
-  description = "List of scope values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.scope]
+  description = "Map of scope values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.scope }
 }
 output "container_registry_webhooks_service_uri" {
-  description = "List of service_uri values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.service_uri]
+  description = "Map of service_uri values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.service_uri }
 }
 output "container_registry_webhooks_status" {
-  description = "List of status values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.status]
+  description = "Map of status values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.status }
 }
 output "container_registry_webhooks_tags" {
-  description = "List of tags values across all container_registry_webhooks"
-  value       = [for k, v in azurerm_container_registry_webhook.container_registry_webhooks : v.tags]
+  description = "Map of tags values across all container_registry_webhooks, keyed the same as var.container_registry_webhooks"
+  value       = { for k, v in azurerm_container_registry_webhook.container_registry_webhooks : k => v.tags }
 }
 
